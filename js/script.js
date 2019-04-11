adebc7dad89c7696cca3238f57299305271cc216
Lhourquin <lhourquin@gmail.com>
2019-04-11 15:25:10 +0200

    target wuth const all element html

--- js/script.js
+++ js/script.js
@@ -1,34 +1,9 @@
 'use strict';
 
-let input = document.getElementsByTagName('input')[0];
-let submit = document.getElementsByTagName('input')[1];
-let score = document.getElementsByTagName('div')[3];
-let reset = document.getElementsByTagName('button')[0];
-let player1 = document.getElementsByTagName('button')[1];
-let player2 = document.getElementsBytagName('button')[2];
-let counterPlayer1 = document.getElementsByTagName('div')[0];
-let counterPlayer2 = document.getElementsByTagName('div')[1];
-//let replace = document.createElement('div');
-
-
-
-/*function addScore() {
-        if(input.value = ""){
-        return null;
-        }else {
-        replace.textContent = input.value;
-        score.replaceWith(replace);
-        }
-        deleteInputContent();
-
-}
-
-function deleteInputContent() {
-    input.value = "";
-}
-
-function resetScore() {
-    
-    replace.textContent = 5;
-    score.replaceWith(replace);
-}*/
+const player1 = document.getElementsByTagName('input'[2]);
+const player2 = document.getElementsByTagName('input'[3]);
+const field = document.getElementsByTagName('input'[0]);
+const reset = document.getElementsByTagName('input'[1]);
+const counter1 = document.getAnimations('div'[0]);
+const counter2 = document.getElementsByTagName('div'[1]);
+const count= document.getElementsByTagName('div'[2]);
