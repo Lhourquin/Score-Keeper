e5b4266d407a88909b22d59ab246b0133af17233
Lhourquin <lhourquin@gmail.com>
2019-04-11 10:09:39 +0200

    initilize variable target element html

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let input = document.getElementsByTagName('input')[0];
+let submit = document.getElementsByTagName('input')[1];
+let score = document.getElementsByClassName('score');
+let reset = document.getElementsByClassName('reset');
+let player1 = document.getElementsByClassName('player1');
+let player2 = document.getElementsByClassName('player2');
+let counterPlayer1 = document.getElementsByClassName('counterPlayer1');
+let counterPlayer2 = document.getElementsByClassName('counterPlayer2');
