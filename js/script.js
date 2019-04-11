9b3d0837764f5899fe15e1cb1067d00cfb5e956d
Lhourquin <lhourquin@gmail.com>
2019-04-11 11:13:42 +0200

    create function addScore, i make a test with the console

--- js/script.js
+++ js/script.js
@@ -2,9 +2,20 @@
 
 let input = document.getElementsByTagName('input')[0];
 let submit = document.getElementsByTagName('input')[1];
-let score = document.getElementsByClassName('score');
+let score = document.getElementsByTagName('div')[0];
 let reset = document.getElementsByClassName('reset');
 let player1 = document.getElementsByClassName('player1');
 let player2 = document.getElementsByClassName('player2');
 let counterPlayer1 = document.getElementsByClassName('counterPlayer1');
 let counterPlayer2 = document.getElementsByClassName('counterPlayer2');
+
+function addScore() {
+
+   /* if(input.value = ""){
+        return null; 
+    }/*else{
+        let num = document.createElement('p');
+        num.textContent = input.value;
+        score.replaceWith(num);
+}*/
+}
