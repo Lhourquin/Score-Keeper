2ff6d6f4f8c830292537e02d1fbd8a07598b09f6
Lhourquin <lhourquin@gmail.com>
2019-04-11 14:53:06 +0200

    replace index for target div

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 
 let input = document.getElementsByTagName('input')[0];
 let submit = document.getElementsByTagName('input')[1];
-let score = document.getElementsByTagName('div')[0];
+let score = document.getElementsByTagName('div')[3];
 let reset = document.getElementsByTagName('button')[0];
 let player1 = document.getElementsByTagName('button')[1];
 let player2 = document.getElementsBytagName('button')[2];
-let counterPlayer1 = document.getElementsByTagName('div')[1];
-let counterPlayer2 = document.getElementsByTagName('div')[2];
+let counterPlayer1 = document.getElementsByTagName('div')[0];
+let counterPlayer2 = document.getElementsByTagName('div')[1];
 //let replace = document.createElement('div');
 
 
