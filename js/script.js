59d18a4fd1ad7415e72a0b6b4ec9ad59c54aac9f
Lhourquin <lhourquin@gmail.com>
2019-04-11 23:04:51 +0200

    create variable divCounter1/2 , resolve the last bug with function, function addCounter1 and addCounter2 ok

--- js/script.js
+++ js/script.js
@@ -7,24 +7,25 @@ const player2 = document.getElementsByTagName('input')[3];
 const counter1 = document.getElementsByTagName('div')[0];
 const counter2 = document.getElementsByTagName('div')[1];
 const count= document.getElementsByTagName('div')[2];
-let value1;
-let divCounter = document.createElement('div');
-
+let value;
+let divCounter1 = counter1.cloneNode(true);
+let divCounter2 = counter2.cloneNode(true);
 
 function addCounter1() {
-    value1 = player1.value++;
-    value1++;
-    divCounter.textContent = value1;
-    counter1.replaceWith(divCounter);
-    
+    value = player1.value++;
+    value++;
+    divCounter1.textContent = value;
+    counter1.replaceWith(divCounter1);
+    divCounter1.replaceWith(divCounter1); 
+}
+
+function addCounter2() {
+    value = player2.value++;
+    value++;
+    divCounter2.textContent = value;
+    counter2.replaceWith(divCounter2);
+    divCounter2.replaceWith(divCounter2);
 }
 
 player1.onclick = addCounter1;
 player2.onclick = addCounter2;
-
-function addCounter2() {
-    value1 = player2.value++;
-    value1++;
-    divCounter.textContent = value1;
-    counter2.replaceWith(divCounter);
-}
