349e076724893826f1e2044ce3b4e8f2afc3567d
Lhourquin <lhourquin@gmail.com>
2019-04-12 15:11:07 +0200

    initialize variable max function stop when is equal to max

--- js/script.js
+++ js/script.js
@@ -7,36 +7,42 @@ const player2 = document.getElementsByTagName('button')[2];
 const counter1 = document.getElementsByTagName('div')[0];
 const counter2 = document.getElementsByTagName('div')[1];
 let score = document.getElementsByTagName('div')[2];
+let max = 5;
 let value;
 let divCounter1 = counter1.cloneNode(true);
 let divCounter2 = counter2.cloneNode(true);
 let divScore = score.cloneNode(true);
 
 function addCounter1() {
+    
+    if(player1.value == max || player2.value == max){
+    return;
+    }else{
     value = player1.value++;
     value++;
     divCounter1.textContent = value;
     counter1.replaceWith(divCounter1);
     divCounter1.replaceWith(divCounter1); 
-    stopIncrement();
+    }
 }
 
 function addCounter2() {
+    if(player2.value == max || player1.value == max){
+    return;
+    }else{
     value = player2.value++;
     value++;
     divCounter2.textContent = value;
     counter2.replaceWith(divCounter2);
     divCounter2.replaceWith(divCounter2);
-    stopIncrement();
+    }
 }
 
-function stopIncrement() {
-    console.log( (player1.value == field.value && player2.value == field.value)? true : false);
-}
+
 
 /*function resetScore() {
 
-    value = score.value;
+    value = max;
     divScore.textContent = value;
     score.replaceWith(divScore);
 
